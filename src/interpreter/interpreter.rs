dae71fd5fcfefe8506435ead7a87f19430a7635f
emilyaherbert <emilyherbert@cs.umass.edu>
2020-04-10 02:14:29 -0400

    Exp doesnt return env.

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -34,11 +34,11 @@ impl Interpreter {
     }
 
     fn eval_stmt<'a>(
-        &mut self, stmt: &Stmt, env: Env<'a>, arena: &'a Bump,
+        &mut self, stmt: &Stmt, mut env: Env<'a>, arena: &'a Bump,
     ) -> (StmtResult<'a>, Env<'a>) {
         match stmt {
             Stmt::Let { name, named } => {
-                let (named, mut env) = self.eval_exp(named, env, arena);
+                let named = self.eval_exp(named, env, arena);
                 match named {
                     Value::Array { values:_ } => env.add_value(arena, name.to_string(), named),
                     other => env.add_value(arena, name.to_string(), vref_(arena, other))
@@ -46,12 +46,12 @@ impl Interpreter {
                 (srnothing_(), env)
             }
             Stmt::Set { lval, named } => {
-                let (named, env) = self.eval_exp(named, env, arena);
+                let named = self.eval_exp(named, env, arena);
                 let env = self.set_lval(lval, named, env, arena);
                 (srnothing_(), env)
             }
             Stmt::Return { value } => {
-                let (value, env) = self.eval_exp(value, env, arena);
+                let value = self.eval_exp(value, env, arena);
                 (srreturn_(value), env)
             }
             _ => unimplemented!(),
@@ -60,62 +60,55 @@ impl Interpreter {
 
     fn eval_exp<'a>(
         &mut self, exp: &Exp, env: Env<'a>, arena: &'a Bump,
-    ) -> (Value<'a>, Env<'a>) {
+    ) -> Value<'a> {
         match exp {
-            Exp::Number { value } => (vnumber_(*value), env),
+            Exp::Number { value } => vnumber_(*value),
             Exp::Identifier { name } => {
                 let v = env.get_value(name);
                 match v {
-                    Value::Ref { value } => (value.get(), env),
-                    other => (other, env)
+                    Value::Ref { value } => value.get(),
+                    other => other
                 }
             },
             Exp::BinOp { op, e1, e2 } => {
-                let (e1, env1) = self.eval_exp(e1, env, arena);
-                let (e2, env2) = self.eval_exp(e2, env1, arena);
+                let e1 = self.eval_exp(e1, env, arena);
+                let e2 = self.eval_exp(e2, env, arena);
                 match (op, e1, e2) {
                     (Op2::Add, Value::Number { value: v1 }, Value::Number { value: v2 }) => {
-                        (vnumber_(v1 + v2), env2)
+                        vnumber_(v1 + v2)
                     }
                     _ => unimplemented!(),
                 }
             }
             Exp::Array { exps } => {
                 let mut values: Vec<Value<'a>> = vec![];
-                let mut env = env;
                 for v in exps {
-                    let (v, e) = self.eval_exp(v, env, arena);
+                    let v = self.eval_exp(v, env, arena);
                     values.push(v);
-                    env = e;
                 }
-                (varray_(arena, values), env)
+                varray_(arena, values)
             }
             Exp::Index { e1, e2 } => {
-                let (array, env1) = self.eval_exp(e1, env, arena);
-                let (index, env2) = self.eval_exp(e2, env1, arena);
+                let array = self.eval_exp(e1, env, arena);
+                let index = self.eval_exp(e2, env, arena);
                 match (array, index) {
                     (Value::Array { values }, Value::Number { value }) => {
                         if (value >= 0.0) && (value <= usize::max_value() as f64) {
-                            (values.borrow()[value as usize].to_owned(), env2)
+                            values.borrow()[value as usize].to_owned()
                         } else {
-                            (vundefined_(), env2)
+                            vundefined_()
                         }
                     }
                     _ => panic!("Expected array and index."),
                 }
             }
-            Exp::Function { params, body } => (
-                vclos_(arena, env.clone(), params.to_vec(), body.to_vec()),
-                env,
-            ),
+            Exp::Function { params, body } => vclos_(arena, env.clone(), params.to_vec(), body.to_vec()),
             Exp::FunApp { fun, fun_args } => {
-                let (clos, env) = self.eval_exp(fun, env, arena);
+                let clos = self.eval_exp(fun, env, arena);
                 let mut fun_args2: Vec<Value> = vec![];
-                let mut env = env;
                 for a in fun_args.into_iter() {
-                    let (a, e) = self.eval_exp(&a, env, arena);
+                    let a = self.eval_exp(&a, env, arena);
                     fun_args2.push(a);
-                    env = e;
                 }
                 match clos {
                     Value::Clos {
@@ -131,8 +124,8 @@ impl Interpreter {
                                 fun_env.add_value(arena, p.to_string(), vref_(arena, a));
                             });
                         match self.eval_stmts(body, fun_env, arena) {
-                            StmtResult::Return { value } => (value, env),
-                            StmtResult::Nothing => (vundefined_(), env),
+                            StmtResult::Return { value } => value,
+                            StmtResult::Nothing => vundefined_(),
                         }
                     }
                     _ => panic!("Expected env."),
@@ -154,7 +147,7 @@ impl Interpreter {
                 let name = self.get_id(e);
                 match env.get_value(&name) {
                     Value::Array { values } => match self.eval_exp(index, env, arena) {
-                        (Value::Number { value }, env) => {
+                        Value::Number { value } => {
                             if (value >= 0.0) && (value <= usize::max_value() as f64) {
                                 std::mem::replace(&mut values.borrow_mut()[value as usize], rval);
                             }
